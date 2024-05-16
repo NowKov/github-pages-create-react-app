a9d91491aaec656c9406eea9c5d098255d5ddfc1
NowKov <genesis-45@yandex.ru>
2024-05-16 14:06:55 +0300

    Comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+function App(){
+    return(
+      
+      <body>
+        <div className="watermelon">
+            <div className="cut-1"></div>
+            <div className="seed1"></div>
+            <div className="seed2"></div>
+            <div className="seed3"></div>
+            <div className="seed4"></div>
+            <div className="seed5"></div>
+            <div className="cut-2"></div>
+        </div>
+      
+        <script src="" async defer></script>
+      </body>
+    );
 }
-
 export default App;
